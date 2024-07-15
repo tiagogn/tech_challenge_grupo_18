2cfdd2c74c1f660f8bd1321a431377c4e2d52074
roseanesc <roseane.sousa@grupoboticario.com.br>
2024-07-15 18:24:40 -0300

    Finalizando estrutura hexagonal

--- src/main/kotlin/br/com/fiap/lanchonete/LanchoneteApplication.kt
+++ src/main/kotlin/br/com/fiap/lanchonete/LanchoneteApplication.kt
@@ -1,4 +1,4 @@
-package br.com.fiap.lanchonete.core
+package br.com.fiap.lanchonete
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
