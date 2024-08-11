85ebd221ba6889250a0c3dd429e0ed5f26c5333f
Tiago Gonçalves <tiagogn@gmail.com>
2024-08-11 12:52:08 -0300

    constraint para não permitir produto repitido

--- src/main/resources/db/migration/V1_1__create_table_produto.sql
+++ src/main/resources/db/migration/V1_1__create_table_produto.sql
@@ -1,6 +1,10 @@
-create table if not exists "produto"(
-    id uuid primary key,
-    nome text not null,
-    preco numeric not null,
-    categoria text not null
+create table if not exists "produto"
+(
+    id        uuid primary key,
+    nome      text    not null,
+    preco     numeric not null,
+    categoria text    not null
 );
+
+alter table produto
+    add constraint unique_produti unique (nome, preco, categoria);
